6958d650cce8d984a9bcb0c733ebe9a2df6b7d04
Pedro Abreu <abreu223@hotmail.com>
2018-09-20 15:07:46 -0400

    Fuel as State nat

--- src/Refine2.v
+++ src/Refine2.v
@@ -25,7 +25,7 @@ Definition nonDet_list : Eff [Choice] (list nat) :=
 Definition det_list: Eff [Choice] _ :=
      (pure (repeat 10 11)).
 
-Fixpoint denotes_choice' {v} {effs} (s1: Eff (Choice :: effs) v) (s2: Eff [] v): Prop :=
+Fixpoint denotes_choice' `(s1: Eff (Choice :: effs) v) (s2: Eff [] v): Prop :=
   match s2 with
   | Impure u k => False_rect _ (Union_empty _ u)
   | Pure a2 =>
@@ -39,13 +39,13 @@ Fixpoint denotes_choice' {v} {effs} (s1: Eff (Choice :: effs) v) (s2: Eff [] v):
     end
   end.
 
-Fixpoint denotes_choice {v} {effs} (s1: Eff (Choice :: effs) v) (s2: Eff effs v): Prop :=
+Fixpoint denotes_choice `(s1: Eff (Choice :: effs) v) (s2: Eff effs v): Prop :=
   match s2 with
   | Impure u k => exists a, denotes_choice s1 (k a) /\ ~ In Choice effs
   | Pure a2 => denotes_choice' s1 (Pure a2)
   end.
 
-Fixpoint denotes_comp' {v} {effs} (s1: Eff (Comp :: effs) v): Eff [] v -> Prop :=
+Fixpoint denotes_comp' `(s1: Eff (Comp :: effs) v): Eff [] v -> Prop :=
   fun s2 => match s2 with
   | Impure u _ => False_rect _ (Union_empty _ u)
   | Pure a2 =>
@@ -59,11 +59,26 @@ Fixpoint denotes_comp' {v} {effs} (s1: Eff (Comp :: effs) v): Eff [] v -> Prop :
     end
   end.
 
-Fixpoint denotes_comp {v} {effs} (s1: Eff (Comp :: effs) v) (s2: Eff effs v): Prop :=
+Fixpoint denotes_comp `(s1: Eff (Comp :: effs) v) (s2: Eff effs v): Prop :=
   match s2 with
   | Impure u k => exists a, denotes_comp s1 (k a) /\ ~ In Comp effs
   | Pure a2 => denotes_comp' s1 (Pure a2)
   end.
+
+(*
+Fixpoint denotes_state `(s1 s2: Eff ((State x) :: effs) v): Prop :=
+  match s1, s2 with
+  | Pure a1, Pure a2 => a1 = a2
+  | Impure u1 k1, Impure u2 k2 =>
+    match decomp u1, decomp u2 with
+      | inl p1, inl p2 => exists a1 a2, denotes_state (k1 a1) (k2 a2) /\ p1 = p2
+      | inr u1', inr u2' => exists a1 a2, denotes_state (k1 a1) (k2 a2)
+      | _, _ => False
+      end
+  | _, _ => False 
+  end.
+*)
+
 
 Lemma reduce_nonDet: 
   denotes_choice nonDet_list (pure (repeat 10 11)).
@@ -75,13 +90,13 @@ Proof.
 Qed.
 
 Lemma nonEffect_Denotes: forall v effs (l: v),
-    @denotes_choice _ effs (pure l) (pure l).
+    @denotes_choice effs _ (pure l) (pure l).
 Proof.
   reflexivity.
 Qed.
 
 Lemma nonEffect_pure: forall v x (l: v),
-    @denotes_choice _ nil (pure l) x -> x = (pure l).
+    @denotes_choice nil _ (pure l) x -> x = (pure l).
 Proof.
   destruct x; intros; simpl in *.
   - subst.
@@ -90,7 +105,7 @@ Proof.
 Qed.
 
 Lemma pure_nonEffect: forall v (l: v) x,
-    x = (pure l) -> @denotes_choice _ nil (pure l) x.
+    x = (pure l) -> @denotes_choice nil _ (pure l) x.
 Proof.
   intros; subst.
   reflexivity.
@@ -108,7 +123,8 @@ Proof.
   apply reduce_nonDet.
 Qed.
 
-Fixpoint denote_aexp {effs} (a:aexp): Eff ([State state]++effs) nat :=
+Fixpoint denote_aexp `{Member (State state) effs}
+         (a:aexp): Eff effs nat :=
   match a with
   | ANum n => pure n
   | AId x => (st <- send Get;
@@ -117,7 +133,8 @@ Fixpoint denote_aexp {effs} (a:aexp): Eff ([State state]++effs) nat :=
                                  => denote_aexp a1 ;; denote_aexp a2
   end.
 
-Fixpoint denote_bexp {effs} (b:bexp): Eff ([State state]++effs) bool :=
+Fixpoint denote_bexp `{Member (State state) effs}
+         (b:bexp): Eff ([State state]++effs) bool :=
   match b with
   | BTrue => pure true
   | BFalse => pure false
@@ -131,7 +148,8 @@ Fixpoint denote_bexp {effs} (b:bexp): Eff ([State state]++effs) bool :=
                  pure (negb b''))
   end.
 
-Fixpoint denote_imp {effs} (c: com): Eff ([State state]++effs) state :=
+Fixpoint denote_imp `{Member (State state) effs}
+         (c: com): Eff ([State state]++effs) state :=
   match c with
   | SKIP => (st <- send Get; pure st)
   | CAss x ax => (a <- denote_aexp ax;
@@ -150,6 +168,7 @@ Inductive NDaexp: Type :=
 | NDnum : (Choice nat) -> NDaexp.
 
 Inductive NDbexp : Type :=
+| NDBool: (Choice bool) -> NDbexp
 | NBTrue : NDbexp
 | NBFalse : NDbexp
 | NBEq : NDaexp -> NDaexp -> NDbexp
@@ -165,15 +184,33 @@ Inductive NDcom : Type :=
 | NDCWhile : NDbexp -> NDcom -> NDcom.
 
 
-Fixpoint denote_NDaexp (na: NDaexp): Eff [State state; Choice] nat :=
+(*
+Polymorphic Inductive Fuel {a: Type} (t: a -> a): a -> Type :=
+| Consume : forall (v: a), Fuel t (t v).
+*)
+Definition Fuel: Type -> Type := State nat.
+
+Definition step_fuel `{Member Fuel effs} `(f: a -> Eff effs v) (x: a) (default: v):
+  (Eff effs v) :=
+  fuel <- send Get;
+     (match fuel with
+      | O => pure default
+      | S n' => send (Put n');; f x
+      end).
+
+Fixpoint denote_NDaexp `{Member Choice effs} `{Member (State state) effs} `{Member Fuel effs}
+         (na: NDaexp): Eff effs nat :=
   match na with
   | Daexp a => denote_aexp a
   | NDnum (Pick p) => n <- send (Pick (fun (x: nat) => p x));
               pure n
   end.
 
-Fixpoint denote_NDbexp (nb:NDbexp): Eff [State state; Choice] bool :=
+Fixpoint denote_NDbexp `{Member Choice effs} `{Member (State state) effs} `{Member Fuel effs}
+         (nb:NDbexp): Eff effs bool :=
   match nb with
+  | NDBool (Pick p) => b <- send (Pick (fun (b: bool) => p b));
+                        pure b
   | NBTrue => pure true
   | NBFalse => pure false
   | NBEq a1 a2 | NBLe a1 a2 => (x1 <- (denote_NDaexp a1) ;
@@ -186,18 +223,30 @@ Fixpoint denote_NDbexp (nb:NDbexp): Eff [State state; Choice] bool :=
                  pure (negb b''))
   end.
 
-Fixpoint denote_NDimp (nc: NDcom): Eff [State state; Choice] state :=
+Fixpoint denote_NDimp `{Member Choice effs} `{Member (State state) effs} `{Member Fuel effs}
+         (nc: NDcom): Eff effs unit :=
   match nc with
-  | NDCSkip => (st <- send Get; pure st)
+  | NDCSkip => pure tt
   | NDCAss x ax => (a <- denote_NDaexp ax;
                    st <- send Get;
-                   pure (t_update st x a))
+                   send (Put (t_update st x a));;
+                   pure tt)
+
   | NDCIf b c1 c2 => b' <- denote_NDbexp b;
-                                 if (b':bool)
-                                 then denote_NDimp c1
-                                 else  denote_NDimp c2
+                            if (b':bool)
+                            then denote_NDimp c1
+                            else  denote_NDimp c2
   | NDCSeq c1 c2 => denote_NDimp c1 ;; denote_NDimp c2
-  | NDCWhile b c' => st <- send Get; pure st
+  | NDCWhile b c' => fuel' <- send Get;
+                      (fix run_fuel n :=
+                         b' <- denote_NDbexp b;
+                         if (b': bool)
+                         then match n with
+                              | O => pure tt
+                              | S n => denote_NDimp c';;
+                                        run_fuel n
+                              end
+                         else pure tt) fuel'
   end.
 
 Definition effect_swap `(e: Eff ([eff1; eff2]++effs) v) : Eff ([eff2; eff1]++effs) v.
@@ -218,15 +267,20 @@ Proof.
 Defined.
 
 Notation "⇄ x" := (effect_swap x) (at level 50).
-Notation "⟦ c ⟧" := (denote_NDimp c) (at level 40).
+Notation "⟦ c ⟧" := (@denote_NDimp [Choice; State state; Fuel] _ _ _ c) (at level 40).
+
+(* e1 refines e2 if forall t \in [| e2 |] -> t \in [| e1 |] *)
+Definition refines_imp {v} {effs} e1 e2 := forall t, @denotes_choice v effs e2 t -> @denotes_choice v effs e1 t.
+
+
 
 Definition plusNDet :=
-  ⇄ ⟦ NDCAss "X" (NDnum (Pick (fun x => x <= 2))) ⟧.
+  ⟦ NDCAss "X" (NDnum (Pick (fun x => x <= 2))) ⟧.
 
 Definition plusDet :=
-  ⇄ ⟦ NDCAss "X" (Daexp (ANum 2)) ⟧.
+  ⟦ NDCAss "X" (Daexp (ANum 2)) ⟧.
 
-Lemma refines_plusNDet_plusDet:
+Lemma refines_plus:
   refines plusNDet plusDet.
 Proof.
   unfold refines.
@@ -239,6 +293,35 @@ Proof.
      exists x0.
      destruct H.
      split; auto.
+Qed.
+Definition nonDet_assign:=
+  ⟦ NDCWhile (NDBool (Pick (fun b => b = true))) (NDCAss "X" (Daexp (ANum 3))) ⟧.
+
+Definition det_assign:=
+  ⟦ NDCWhile NBTrue (NDCAss "X" (Daexp (ANum 2))) ⟧.
+
+Lemma refines_assign:
+  refines nonDet_assign det_assign.
+Proof.
+  unfold refines.
+  intros.
+  induction t.
+  -- destruct H.
+     simpl in *; eauto.
+     induction x.
+     --- simpl in *.
+         exists 0; simpl.
+         exists true; split; eauto.
+     --- simpl in *.
+         apply IHx.
+         destruct H.
+         destruct H.
+         auto.
+  -- simpl in *.
+     destruct H.
+     destruct H.
+     eexists.
+     split; eauto.
 Qed.
 
 (* 1 V Denotes polimorphic on the rest of the effects
